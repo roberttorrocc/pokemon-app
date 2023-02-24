4022bacd8425bf1052589ad80f0f59f3a2bc1302
roberttorrocc <roberttorro@capitole-consulting.com>
2023-02-24 09:58:44 +0100

    Borrado de cosas que no se utilizan

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCharacter } from '@/components/slice/characterSlice';
-import { RootState } from '@/components/store/store';
+
 import CharacterList from '../components/CharacterList'
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const { data, loading, error } = useSelector(
-    (state: RootState) => state.episodios
-  );
-
-  useEffect(() => {
-    dispatch(fetchCharacter() as any);
-  }, [dispatch]);
-
   return (
     <>
       <div>
