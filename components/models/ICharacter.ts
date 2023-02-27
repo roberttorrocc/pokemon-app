6e2e9742bf9b43698e08763d650e12e4ba34833a
roberttorrocc <roberttorro@capitole-consulting.com>
2023-02-27 10:09:54 +0100

    Mas valores en el type ICharacter

--- components/models/ICharacter.ts
+++ components/models/ICharacter.ts
@@ -1,6 +1,12 @@
 export interface ICharacter {
   id: number;
   name: string;
+  status: string;
+  species: string;
+  episode: string[];
+  origin: {
+    name: string;
+    url: string;
+  };
   image: string;
-
 }
