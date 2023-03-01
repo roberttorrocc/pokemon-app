1433535c9b4bc931c4ba10998fbd557aba5216f5
roberttorrocc <roberttorro@capitole-consulting.com>
2023-03-01 09:42:33 +0100

    Buscador funcional, sin css

--- components/CharacterInformation.tsx
+++ components/CharacterInformation.tsx
@@ -1,9 +1,23 @@
 import { ICharacter } from "@/components/models/ICharacter";
+import {IEpisode} from 'components/models/IEpisode'
 import {fetchEpisode} from 'components/fetchs/fetchEpisodes'
-import Router from "next/router";
+
+
+/*async function asd(params:string ) {
+  return await fetchEpisode(params);
+}
+//const promiseEpisodes:IEpisode[] = await fetchEpisode("asd");*/
 
 export function CharacterInformation({ character }: { character: ICharacter }) {
-  return (
+   /*let episodes: IEpisode[] = asd(character.episode[1]);
+    //character.episode.map()
+    character.episode.forEach(element => {
+      fetchEpisode(element);
+    });*/
+
+  
+  
+    return (
     <div className="w-2/3 bg-gray-800 text-white z-50 py-4 px-8 rounded-lg">
       <img src={character.image} />
       <h1 className="text-4xl">{character.name}</h1>
@@ -11,7 +25,10 @@ export function CharacterInformation({ character }: { character: ICharacter }) {
         <span>Status: {character.status}</span>
         <span>species: {character.species}</span>
         <span>Origin: {character.origin.name}</span>
-        
+        {character.episode?.map((episode: string, index: number) => (
+            <span>{(episode)}</span>
+         
+        ))}
       </div>
     </div>
   );
--- components/CharacterList.tsx
+++ components/CharacterList.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { dispatchDataCharacter } from "components/slice/characterSlice";
 import { RootState } from "components/store/store";
 import { ICharacter } from "components/models/ICharacter";
-import {CharacterCard} from './CharacterCard'
+import { CharacterCard } from "./CharacterCard";
 
 function CharacterList() {
   const dispatch = useDispatch();
@@ -15,17 +15,27 @@ function CharacterList() {
     dispatch(dispatchDataCharacter() as any);
   }, [dispatch]);
 
+  const [characterList, setCharacterList] = useState("");
+  const filtredList = data.filter((c) =>
+    c.name.toLocaleLowerCase().includes(characterList.toLocaleLowerCase())
+  );
+
   return (
-    
+    <>
+      <input
+        type="search"
+        value={characterList}
+        onChange={(e) => setCharacterList(e.target.value)}
+      />
       <div className="mx-auto grid max-w-6xl  grid-cols-1 gap-6 p-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {loading && <p>Loading...</p>}
         {error && <p>{error}</p>}
         {!loading &&
-          data.map((character: ICharacter, index: number) => (
-            <CharacterCard key={index} character={character}/> 
+          filtredList.map((character: ICharacter, index: number) => (
+            <CharacterCard key={index} character={character} />
           ))}
       </div>
-    
+    </>
   );
 }
 
